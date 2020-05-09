2032025c10cda6b2c847be3ae8ec9e60f3f5dfef
GarySimons <garycsimons@gmail.com>
2020-05-09 11:15:22 +0000

    Tested code

--- todo/tests_forms.py
+++ todo/tests_forms.py
@@ -8,7 +8,3 @@ class TestToDoItemForm(TestCase):
         form = ItemForm({'name': 'Create Tests'})
         self.assertTrue(form.is_valid())
 
-    def test_correct_message_for_missing_name(self):
-        form = ItemForm({'name': ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['name'], [u'This field is required.'])
