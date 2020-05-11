85dfefec7c063f84fa73da2f0e395cccaa9d357a
GarySimons <garycsimons@gmail.com>
2020-05-11 05:18:43 +0000

    Getting errors. Asking me to put 'localhost' in ALLOWED_HOSTS. Not working so have '*' to allow any host. Now works

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sj8u)q#360f5l4)n4*xbnj15e)jeav@*)7vh=b*rp_zx!u$*lh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['gcs-django-todo.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,14 +80,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-# #     }
-#}
-
-DATABASES = {'default': dj_database_url.parse("postgres://ltohqgbdhazmvc:bed7b6a8b55fcbb6ba3257915cdefdad3da99e7e82a2ae3729e82a0a0e521f5b@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d476mmbrktojum")}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:    
+    DATABASES = {'default': dj_database_url.parse("postgres://ltohqgbdhazmvc:bed7b6a8b55fcbb6ba3257915cdefdad3da99e7e82a2ae3729e82a0a0e521f5b@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d476mmbrktojum")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
